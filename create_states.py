c9cc44e8f0c43455882812687aa60b637c5534d2
AlexZavr <alex.zavr.27100@mail.ru>
2020-02-26 12:55:21 +0400

    Изменили функцию .product() на .combinations_with_replacement()

--- create_states.py
+++ create_states.py
@@ -2,36 +2,39 @@ import itertools
 
 
 def get_lots_of_fragments(amount_of_demands, fragments_in_class):
-    return set(itertools.product(range(1, fragments_in_class + 1), repeat=amount_of_demands))
+    return set(itertools.combinations_with_replacement(range(1, fragments_in_class + 1), amount_of_demands))
 
 
-# число приборов
-M = 10  # int(input("M = "))
-# число фрагментов в 1м классе
-a = 4  # int(input("a = "))
-# число фрагментов во 2м классе
-b = 3  # int(input("b = "))
-# максимальное число требований 1го класса, которое может быть на приборах
-x = M // a
-# максимальное число требований 2го класса, которое может быть на приборах
-y = M // b
+def main():
+    all_states = set()
+    # число приборов
+    M = 10  # int(input("M = "))
+    # число фрагментов в 1м классе
+    a = 4  # int(input("a = "))
+    # число фрагментов во 2м классе
+    b = 3  # int(input("b = "))
+    # максимальное число требований 1го класса, которое может быть на приборах
+    x = M // a
+    # максимальное число требований 2го класса, которое может быть на приборах
+    y = M // b
 
-X = set()
-Y = set()
-all_states = set()
+    for i in range(x + 1):
+        for j in range(y + 1):
+            # число фрагментов которое может находиться на обслуживании
+            z = a * i + b * j
+            if M < z:
+                continue
 
-for i in range(x + 1):
-    for j in range(y + 1):
-        # число фрагментов которое может находиться на обслуживании
-        z = a * i + b * j
-        if M < z:
-            continue
-        print('i =', i, 'j =', j)
-        X = sorted(get_lots_of_fragments(i, a))
-        print(X)
-        Y = sorted(get_lots_of_fragments(j, b))
-        print(Y)
-        all_states.update(itertools.product(X, Y))
+            print('i =', i, 'j =', j)
+            X = sorted(get_lots_of_fragments(i, a))
+            # print(X)
+            Y = sorted(get_lots_of_fragments(j, b))
+            # print(Y)
+            all_states.update(itertools.product(X, Y))
 
-for state in all_states:
-    print(state)
+    for state in all_states:
+        print(state)
+
+
+if __name__ == '__main__':
+    main()
