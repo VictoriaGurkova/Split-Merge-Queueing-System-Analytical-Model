c9d659f391bb0919750cea82930df278ca95d390
ViktoriaGurkova <viktorija.gurkova@mail.ru>
2020-04-05 23:40:00 +0400

    Added achivables and intensities(there is dublication)

--- create_states.py
+++ create_states.py
@@ -27,12 +27,9 @@ def get_lots_of_fragments(amount_of_demands, fragments_in_class):
     return list(itertools.combinations_with_replacement(range(1, fragments_in_class + 1), amount_of_demands))
 
 
-def get_all_state_with_queues(server_states: set,
-                              queue_capacity: list,
-                              M, a, b):
+def get_all_state_with_queues(server_states: set, queue_capacity: list, M, a, b):
     states = []
-    # так симпатичнее
-    queue_states = set(itertools.product(range(queue_capacity[0] + 1), range(queue_capacity[0] + 1)))
+    queue_states = set(itertools.product(range(queue_capacity[0] + 1), range(queue_capacity[1] + 1)))
 
     for q_state in queue_states:
         for server_state in server_states:
@@ -40,7 +37,6 @@ def get_all_state_with_queues(server_states: set,
             # гененируется слишком много лишних состояний, которые потом фильтруем
             # но пусть пока будет так
             if is_possible_state(q_state, server_state, M, a, b):
-                # зачем тут был dict? для красоты вывода на консоль?
                 states.append((q_state, server_state))
     return states
 
@@ -54,16 +50,11 @@ def is_possible_state(q_state, state, M, a, b):
     return True
 
 
-def get_achievable_states(current_state: tuple,
-                          M: int,
-                          a: int, b: int,
-                          queue_capacity: list):
+def get_achievable_states(current_state: tuple, M: int, a: int, b: int, queue_capacity: list,
+                          lambda1: float, lambda2: float, mu: float):
     print('#' * 100)
     print('Рассмотрим состояние ' + pretty_state(current_state))
-    # не будем делать никаких проверок, это снова слишком долго
-    # быстрее построить эти состояние перебирая все события, которые могут произойти
-    # не стал разбивать на отдельные функции из-за необходимости таскать кучу параметров
-    # так как все парметры не в классе
+
     capacity1 = queue_capacity[0]
     capacity2 = queue_capacity[1]
 
@@ -71,7 +62,7 @@ def get_achievable_states(current_state: tuple,
     q1 = current_state[0][0]
     print('q1 = ', q1)
     q2 = current_state[0][1]
-    print('q1 = ', q2)
+    print('q2 = ', q2)
     server_state = current_state[1]
     print('server_state = ', server_state)
 
@@ -80,42 +71,87 @@ def get_achievable_states(current_state: tuple,
 
     # ПОСТУПЛЕНИЕ
     # первый класс - поступление требований
-    # есть приборов не хватает, то идем в очередь, если размер очереди позволят
+    # если приборов не хватает, то идем в очередь, если размер очереди позволяет
     print("ПОСТУПЛЕНИЕ")
     if q1 != capacity1:
         # идем в очередь
         if number_of_free_servers < a:
             new_state = create_state(q1 + 1, q2, server_state[0], server_state[1])
-            print('Поступление требования первого класса в очередь  с какой-то интенсивностью и переход в стояние ',
-                  new_state, "  pretty-form = ", pretty_state(new_state))
+            print(f'Поступление требования первого класса в очередь с интенсивностью {lambda1} и переход в стояние ',
+                  pretty_state(new_state))
         # идем на приборы - добавление во множество новое требование - т.е. новое количество фрагментов
         else:
-            updated_first_demands_tasks = server_state[0]
-            updated_first_demands_tasks = updated_first_demands_tasks + (a,)
-            new_state = create_state(q1, q2, updated_first_demands_tasks, server_state[1])
-            print('Поступление требования первого класса и немедленное начало его обслуживания'
-                  '  с какой-то интенсивностью и переход в стояние ',
-                  new_state, "  pretty-form = ", pretty_state(new_state))
+            updated_second_demands_tasks = server_state[0]
+            updated_second_demands_tasks = updated_second_demands_tasks + (a,)
+            new_state = create_state(q1, q2, updated_second_demands_tasks, server_state[1])
+            print(f'Поступление требования первого класса с интенсивностью {lambda1} и немедленное начало его '
+                  'обслуживания и переход в состояние ', pretty_state(new_state))
+    else:
+        print("Очередь заполнена - требование потерялось")
+
+    if q2 != capacity2:
+        # идем в очередь
+        if number_of_free_servers < b:
+            new_state = create_state(q1, q2 + 1, server_state[0], server_state[1])
+            print(f'Поступление требования второго класса в очередь с интенсивностью {lambda2} и переход в стояние ',
+                  pretty_state(new_state))
+        # идем на приборы - добавление во множество новое требование - т.е. новое количество фрагментов
+        else:
+            updated_second_demands_tasks = server_state[1]
+            updated_second_demands_tasks = updated_second_demands_tasks + (b,)
+            new_state = create_state(q1, q2, server_state[0], updated_second_demands_tasks)
+            print(f'Поступление требования второго класса с интенсивностью {lambda2} и немедленное начало его '
+                  ' обслуживания и переход в состояние ', pretty_state(new_state))
     else:
         print("Очередь заполнена - требование потерялось")
 
     # ЗАВЕРШЕНИЕ ОБСЛУЖИВАНИЯ
-    # первый класс - завершение обслуживания одного из фрагментов или даже целого требования
-    # на приборах есть несколько требований первого класса,
-    # в каждом требовании фрагменты, если на приборах
     print('УХОД')
-    for number_of_lost_tasks_in_demand in server_state[0]:
-        # обнаружил требование первого класса, в нем активны tasks фрагментов
+    # 1й класс
+    for state_id, number_of_lost_tasks_in_demand in enumerate(server_state[0]):
+        updated_first_demands_tasks = list(server_state[0])
+        copy_q1 = q1
         if number_of_lost_tasks_in_demand == 1:
-            print('Остался один фрагмент - нужно удалить все требование после того как фрагмент уйдет')
+            updated_first_demands_tasks.pop(state_id)
+            if q1:
+                updated_first_demands_tasks = updated_first_demands_tasks + [a]
+                copy_q1 = q1 - 1
+            new_state = create_state(copy_q1, q2, tuple(updated_first_demands_tasks), server_state[1])
+            print(f'Завершение обслуживания всего требования первого класса с интенсивностью {mu}',
+                  'и переход в состояние ', pretty_state(new_state))
+
         else:
-            print('Остался не единственный фрагмент - можно просто убрать один и уменьшить их число на единицу')
+            leave_intensity = mu * updated_first_demands_tasks[state_id]
+            updated_first_demands_tasks[state_id] -= 1
+            new_state = create_state(q1, q2, tuple(updated_first_demands_tasks), server_state[1])
+            print(f'Завершение обслуживания фрагмента требования первого класса с интенсивностью {leave_intensity}',
+                  'и переход в состояние ', pretty_state(new_state))
+
+    # 2й класс
+    for state_id, number_of_lost_tasks_in_demand in enumerate(server_state[1]):
+        updated_second_demands_tasks = list(server_state[1])
+        copy_q2 = q2
+        if number_of_lost_tasks_in_demand == 1:
+            updated_second_demands_tasks.pop(state_id)
+            if q2:
+                updated_second_demands_tasks = updated_second_demands_tasks + [b]
+                copy_q2 = q2 - 1
+            new_state = create_state(q1, copy_q2, server_state[0], tuple(updated_second_demands_tasks))
+            print(f'Завершение обслуживания всего требования второго класса с интенсивностью {mu}',
+                  'и переход в состояние ', pretty_state(new_state))
+
+        else:
+            leave_intensity = mu * updated_second_demands_tasks[state_id]
+            updated_second_demands_tasks[state_id] -= 1
+            new_state = create_state(q1, q2, server_state[0], tuple(updated_second_demands_tasks))
+            print(f'Завершение обслуживания фрагмента требования второго класса с интенсивностью {leave_intensity}',
+                  'и переход в состояние ', pretty_state(new_state))
 
     return
 
 
 def number_of_free_servers_for_server_state(server_state, M, a, b):
-    number = M - len(server_state[0]) * a + len(server_state[1]) * b
+    number = M - (len(server_state[0]) * a + len(server_state[1]) * b)
     if number < 0:
         raise Exception("Number of free servers for states < 0, it is not correct state")
     return number
@@ -123,39 +159,6 @@ def number_of_free_servers_for_server_state(server_state, M, a, b):
 
 def create_state(q1, q2, first_class, second_class):
     return (q1, q2), (first_class, second_class)
-
-
-def can_achieve(current_state: map, state: map, a: int, b: int):
-    difference1 = 0
-    difference2 = 0
-    # приход требования класса 1
-    if len(state[0]) - len(current_state[0]) == 1 and state[0][-1] == a and len(state[1]) - len(current_state[1]) == 0 \
-            and current_state[1][:] == state[1][:]:
-        difference1 += 1
-    # приход требования класса 2
-    if len(state[1]) - len(current_state[1]) == 1 and state[1][-1] == b and len(state[0]) - len(current_state[0]) == 0 \
-            and current_state[0][:] == state[0][:]:
-        difference2 += 1
-    # уход требования класса 1
-    if len(current_state[0]) - len(state[0]) == 1 and current_state[1][:] == state[1][:]:
-        if current_state[0][:-1] == state[0][:]:
-            difference1 += current_state[0][-1]
-    # уход требования класса 2
-    if len(current_state[1]) - len(state[1]) == 1 and current_state[0][:] == state[0][:]:
-        if current_state[1][:-1] == state[1][:]:
-            difference2 += current_state[1][-1]
-    # завершение обслуживания фргмента требований 1го класса
-    if len(current_state[0]) == len(state[0]) and current_state[1][:] == state[1][:]:
-        for i in range(len(state[0])):
-            if current_state[0][i] - state[0][i] > 0:
-                difference1 += current_state[0][i] - state[0][i]
-    # завершение обслуживания фргмента требований 2го класса
-    if len(current_state[1]) == len(state[1]) and current_state[0][:] == state[0][:]:
-        for i in range(len(state[1])):
-            if current_state[1][i] - state[1][i] > 0:
-                difference2 += current_state[1][i] - state[1][i]
-
-    return (difference1 + difference2) == 1
 
 
 def main():
@@ -174,9 +177,9 @@ def main():
     print('Максимальное число требований 2го класса = ', y)
 
     # вместимость очереди 1го класса
-    capacity1 = 2
+    capacity1 = 5
     # вместимость очереди 2го класса
-    capacity2 = 2
+    capacity2 = 5
 
     lambda1 = 1
     lambda2 = 2
@@ -198,23 +201,19 @@ def main():
     print("Состояния фрагментов на системах (не включая очереди): (вывожу в красивом и не очень виде, чтобы понимать"
           "используемые структуры данных в состоянии "
           "(кортеж или лист или что-то еще, всегда понимаю что и где лежит и по какому индексу могу найти)  ")
-    # Это слишком сложный вариант вывода
-    # for state in all_states:
-    #     print(f"S{list(all_states).index(state)} =", state)
 
     # enumerate создает пары - номер и элемент чего-то
     for state_id, state in enumerate(server_states):
-        print('S' + str(state_id), '= ', pretty_server_state(state), '|    \t', state)
+        print('S' + str(state_id), '= ', pretty_server_state(state))
 
     states = get_all_state_with_queues(server_states, queue_capacity, M, a, b)
     print("\nСостояния системы вместе с очередями:")
 
-    # pprint(all_state_with_queues)
     for state_id, state in enumerate(states):
-        print('S' + str(state_id), '= ', pretty_state(state), '|    \t', state)
+        print('S' + str(state_id), '= ', pretty_state(state))
 
     for state in states:
-        get_achievable_states(state, M, a, b, queue_capacity)
+        get_achievable_states(state, M, a, b, queue_capacity, lambda1, lambda2, mu)
 
 
 if __name__ == '__main__':
