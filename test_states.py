61a87f6b97b35098e18f5a7f77c54a7e260f666f
VictoriaGurkova <viktorija.gurkova@mail.ru>
2021-01-15 14:07:33 +0400

    tests are refactored

--- test_states.py
+++ test_states.py
@@ -3,35 +3,39 @@ import unittest
 from states import QueueingSystem
 
 
+def calculate_data(config):
+    qs = QueueingSystem(config["M"], config["a"], config["b"], config["c1"],
+                        config["c2"], config["l1"], config["l2"], config["mu"])
+    qs.calculate()
+    print(qs.data)
+    return qs.data
+
+
 class TestStates(unittest.TestCase):
 
     def test_case1(self):
-        date = {"M": 4, "a": 2, "b": 3, "cap1": 5, "cap2": 5, "lam1": 1, "lam2": 1, "mu": 3}
+        data = {
+            "config": dict(M=4, a=2, b=3, c1=5, c2=5, l1=1, l2=1, mu=3),
+            "expected_rt": 1.7833,
+            "expected_pf": 0.1122
+        }
 
-        expected_rt = 1.7832
-        expected_pf = 0.1122
-
-        qs = QueueingSystem(date["M"], date["a"], date["b"], date["cap1"],
-                            date["cap2"], date["lam1"], date["lam2"], date["mu"])
-        qs.calculate()
-        print(qs.data)
-
-        self.assertAlmostEqual(qs.data.RT, expected_rt, places=4)
-        self.assertAlmostEqual(qs.data.PF, expected_pf, places=4)
+        result = calculate_data(data["config"])
+        self.assert_equals(result, data)
 
     def test_case2(self):
-        date = {"M": 7, "a": 5, "b": 2, "cap1": 8, "cap2": 8, "lam1": 1.5, "lam2": 1.5, "mu": 3.5}
+        data = {
+            "config": dict(M=7, a=5, b=2, c1=8, c2=8, l1=1.5, l2=1.5, mu=3.5),
+            "expected_rt": 1.9516,
+            "expected_pf": 0.0631
+        }
 
-        expected_rt = 1.9517
-        expected_pf = 0.0631
+        result = calculate_data(data["config"])
+        self.assert_equals(result, data)
 
-        qs = QueueingSystem(date["M"], date["a"], date["b"], date["cap1"],
-                            date["cap2"], date["lam1"], date["lam2"], date["mu"])
-        qs.calculate()
-        print(qs.data)
-
-        self.assertAlmostEqual(qs.data.RT, expected_rt, places=4)
-        self.assertAlmostEqual(qs.data.PF, expected_pf, places=4)
+    def assert_equals(self, result, data):
+        self.assertAlmostEqual(result.RT, data["expected_rt"], places=4)
+        self.assertAlmostEqual(result.PF, data["expected_pf"], places=4)
 
 
 if __name__ == '__main__':
