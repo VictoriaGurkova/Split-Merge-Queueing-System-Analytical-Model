0ca476668b8724c07c76d774fad8e5dfa12d42da
VictoriaGurkova <viktorija.gurkova@mail.ru>
2021-01-15 14:18:06 +0400

    tests data

--- test_states.py
+++ test_states.py
@@ -3,37 +3,36 @@ import unittest
 from states import QueueingSystem
 
 
-def calculate_data(config):
-    qs = QueueingSystem(config["M"], config["a"], config["b"], config["c1"],
-                        config["c2"], config["l1"], config["l2"], config["mu"])
-    qs.calculate()
-    print(qs.data)
-    return qs.data
+def get_data(params):
+    system = QueueingSystem(params["M"], params["a"], params["b"], params["c1"],
+                            params["c2"], params["l1"], params["l2"], params["mu"])
+    system.calculate()
+    print(system.data)
+    return system.data
 
 
 class TestStates(unittest.TestCase):
 
     def test_case1(self):
-        data = {
-            "config": dict(M=4, a=2, b=3, c1=5, c2=5, l1=1, l2=1, mu=3),
+        test_data = {
+            "params": dict(M=4, a=2, b=3, c1=5, c2=5, l1=1, l2=1, mu=3),
             "expected_rt": 1.7833,
             "expected_pf": 0.1122
         }
 
-        result = calculate_data(data["config"])
-        self.assert_equals(result, data)
+        self.compare_results(test_data)
 
     def test_case2(self):
-        data = {
-            "config": dict(M=7, a=5, b=2, c1=8, c2=8, l1=1.5, l2=1.5, mu=3.5),
+        test_data = {
+            "params": dict(M=7, a=5, b=2, c1=8, c2=8, l1=1.5, l2=1.5, mu=3.5),
             "expected_rt": 1.9516,
             "expected_pf": 0.0631
         }
 
-        result = calculate_data(data["config"])
-        self.assert_equals(result, data)
+        self.compare_results(test_data)
 
-    def assert_equals(self, result, data):
+    def compare_results(self, data):
+        result = get_data(data["params"])
         self.assertAlmostEqual(result.RT, data["expected_rt"], places=4)
         self.assertAlmostEqual(result.PF, data["expected_pf"], places=4)
 
