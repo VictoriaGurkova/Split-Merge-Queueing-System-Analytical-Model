02158bfec9e6a5a81790c7824d5a013e34989b3b
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-01-20 13:17:50 +0400

    generator.py file is created

---
+++ generator.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.linalg import expm
+
+from handlers import get_achievable_states
+from logs import log_message
+
+
+def create_generator(states, params):
+    n = len(states)
+    generator = np.zeros((n, n))
+    for i, current_state in enumerate(states):
+        states_and_rates = get_achievable_states(params, current_state)
+        for state, rate in states_and_rates.items():
+            j = states.index(state)
+            generator[i, j] += rate
+
+    for i, row in enumerate(generator):
+        generator[i, i] = -sum(row)
+
+    return generator
+
+
+def get_stationary_distribution(states, params):
+    generator = create_generator(states, params)
+    log_message(f'Q = {generator}')
+    np.savetxt("output/Q.txt", generator, fmt='%0.0f')
+    return expm(generator * 100000000000)[0]
