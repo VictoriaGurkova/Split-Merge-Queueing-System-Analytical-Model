0d9333a6143b6000daa5a884231263ea281e32c4
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-01-22 14:39:51 +0400

    static names experiments

--- experiment/intensity_dependency.py
+++ experiment/intensity_dependency.py
@@ -5,14 +5,15 @@ from experiment.experiment import Experiment
 
 class IntensityDependency(Experiment):
 
+    __NAME = "intensity"
+
     def __init__(self, k=6, init_value=.5, final_value=2):
-        self.name = "intensity"
         self.init_value = init_value
         self.final_value = final_value
 
         self.lambdas = list(np.linspace(init_value, final_value, k))
 
-        super().__init__(self.name, k)
+        super().__init__(IntensityDependency.__NAME, k)
 
     def save_results(self):
         super().save(self.lambdas)
