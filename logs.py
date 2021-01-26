605c572848226b8d39e815b7a1d3cbb5e65d2883
VictoriaGurkova <viktorija.gurkova@mail.ru>
2021-01-26 16:00:41 +0400

    logs are translated

--- logs.py
+++ logs.py
@@ -11,37 +11,37 @@ def log_message(message):
 
 def log_state(current_state):
     logger.debug('=' * 150)
-    logger.debug('Рассмотрим состояние ' + pretty_state(current_state))
+    logger.debug('Consider state ' + pretty_state(current_state))
 
 
 def log_state_config(config):
     logger.debug(f'q1 = {config["q1"]}')
     logger.debug(f'q2 = {config["q2"]}')
-    logger.debug(f'devices state = {config["devices"]}')
-    logger.debug(f'free devices number = {config["free_devices_number"]}')
+    logger.debug(f'Devices state = {config["devices"]}')
+    logger.debug(f'Free devices number = {config["free_devices_number"]}')
 
 
 def log_arrival_in_queue(lambda_, state, class_id):
-    logger.debug(f'Поступление требования {class_id} класса в '
-                 f'очередь с интенсивностью {lambda_} и '
-                 f'переход в стояние  {pretty_state(state)}')
+    logger.debug(f'Arrival of the {class_id} class demand to '
+                 f'the queue with rate {lambda_} and '
+                 f'transition to state  {pretty_state(state)}')
 
 
 def log_arrival_on_devices(lambda_, state, class_id):
-    logger.debug(f'Поступление требования {class_id} класса с '
-                 f'интенсивностью {lambda_} и '
-                 f'немедленное начало его'
-                 f' обслуживания и переход в состояние {pretty_state(state)}')
+    logger.debug(f'Arrival of the {class_id} class demand '
+                 f'with rate {lambda_} and '
+                 f'immediate start of its service and  '
+                 f'transition to state {pretty_state(state)}')
 
 
 def log_leaving_demand(mu, state, class_id):
-    logger.debug(f'Завершение обслуживания всего требования '
-                 f'{class_id} класса с интенсивностью {mu}, и '
-                 f'переход в состояние {pretty_state(state)}')
+    logger.debug(f'Service completion of whole '
+                 f'{class_id} class demand with rate {mu} and '
+                 f'transition to state {pretty_state(state)}')
 
 
 def log_leaving_fragment(leave_intensity, state, class_id):
-    logger.debug(f'Завершение обслуживания фрагмента '
-                 f'требования {class_id} класса с '
-                 f'интенсивностью {leave_intensity}'
-                 f" переход в состояние {pretty_state(state)}")
+    logger.debug(f'Service completion of '
+                 f'{class_id} class demand fragment with '
+                 f'rate {leave_intensity} and '
+                 f'transition to state {pretty_state(state)}')
