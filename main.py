4a2e8d646c7fef9cdd15eaebf4117ced21748090
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-03-25 21:40:31 +0400

    some experiments

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from network_params import Params
 
 if __name__ == '__main__':
     params = Params(mu=3, lambda1=.5, lambda2=1,
-                    servers_number=4,
-                    fragments_numbers=[3, 2],
-                    queues_capacities=[10, 30])
+                    servers_number=5,
+                    fragments_numbers=[2, 3],
+                    queues_capacities=[1, 1])
     calculations = Calculations(params)
 
     calculations.calculate()
